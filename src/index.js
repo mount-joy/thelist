28511abe12de7278252f8d474a1199a340bc11c5
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-10-05 23:32:18 +0100

    rearrange directory structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import ShoppingList from './ShoppingList';
+
+import ShoppingList from './components/ShoppingList';
 import * as serviceWorker from './serviceWorker';
 
-var destination = document.querySelector("#wrapper");
+import './style/index.css';
 
-ReactDOM.render(
-  <div>
-    <p><ShoppingList/></p>
-  </div>,
-  destination
-);
+const destination = document.querySelector("#wrapper");
+ReactDOM.render(<ShoppingList />, destination);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
