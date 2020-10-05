31a51d0b024589bec5d6e16da4d24ec7645eb620
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-10-05 23:59:48 +0100

    make ListItems a functional component

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -1,20 +1,11 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class ListItems extends Component {
-    createTasks(item) {
-        return <li key={item.key}>{item.text}</li>
-    }
-
-    render() {
-        var Entries = this.props.entries;
-        var listItems = Entries.map(this.createTasks);
-
-        return (
-            <ul>
-                {listItems}
-            </ul>
-        );
-    }
-}
+const ListItems = ({ entries }) => (
+  <ul>
+    {entries.map(item => (
+      <li key={item.key}>{item.text}</li>
+    ))}
+  </ul>
+);
 
 export default ListItems;
