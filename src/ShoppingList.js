6681bd1c1825b184aa679d063c9b738534fdf4a0
LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>
2020-10-16 09:53:05 +0100

    made filteredItems more succinct

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -30,9 +30,7 @@ class ShoppingList extends Component {
   }
 
   deleteItem(key){
-    var filteredItems = this.state.items.filter(function (item) {
-      return (item.key !== key);
-    });
+    const filteredItems = this.state.items.filter(item => item.key != key);
 
     this.setState({
       items: filteredItems
