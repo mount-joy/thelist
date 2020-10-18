c773768a1a3904f2194fb85b3c930be69c12ed2c
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-10-18 11:31:33 +0100

    stop using reserved keyword

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const ListItems = ({ entries, delete }) => (
+const ListItems = ({ entries, deleteItem }) => (
   <ul>
     {entries.map(({ key, text }) => (
-      <li key={key}>{text} <FontAwesomeIcon icon={faTrash} onClick={() => delete(key)} /></li>
+      <li key={key}>{text} <FontAwesomeIcon icon={faTrash} onClick={() => deleteItem(key)} /></li>
     ))}
   </ul>
 );
