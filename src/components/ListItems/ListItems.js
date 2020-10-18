d63baed76bd16e2e4232c9f55dc37ab0e315e3f0
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-10-18 12:46:55 +0100

    add pointer cursor on hover for delete button

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
+import styles from './styles.module.css';
+
 const ListItems = ({ entries, deleteItem }) => (
   <ul>
     {entries.map(({ key, text }) => (
-      <li key={key}>{text} <FontAwesomeIcon icon={faTrash} onClick={() => deleteItem(key)} /></li>
+      <li key={key}>
+        {text}
+        {' '}
+        <FontAwesomeIcon
+          icon={faTrash}
+          onClick={() => deleteItem(key)}
+          className={styles.deleteIcon}
+        />
+      </li>
     ))}
   </ul>
 );
