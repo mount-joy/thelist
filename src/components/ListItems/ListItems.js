96337c1ee813737d74aecd3dcd479e538c6edec2
LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>
2020-10-23 12:13:10 +0100

    NewItem input focus updated
    Font colour was updated to appear the same when typing.
    
    Also altered margin of list ul

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -5,7 +5,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import styles from './styles.module.css';
 
 const ListItems = ({ entries, deleteItem }) => (
-  <ul>
+  <ul className={styles.list}>
     {entries.map(({ key, text }) => (
       <li key={key}
           className={styles.listItem}
