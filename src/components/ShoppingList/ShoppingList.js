30a6d08b26aec2ff62aeb59375811328cf584bc4
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-10-27 08:39:50 +0000

    Make shopping list a functional component (#16)

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,58 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 import ListItems from '../ListItems';
 import styles from './styles.module.css';
 
-class ShoppingList extends Component {
-  constructor(props) {
-    super(props);
-    this.addItem = this.addItem.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.state = { items: [], text: '' };
-  }
+const ShoppingList = () => {
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
 
-  addItem(e) {
-    const { text } = this.state;
+  const addItem = (e) => {
+    e.preventDefault();
+
     if (text !== '') {
       const newItem = {
         text,
         key: Date.now(),
       };
 
-      this.setState((prevState) => ({
-        items: prevState.items.concat(newItem),
-      }));
-
-      this.setState({ text: '' });
+      setItems((prevState) => prevState.concat(newItem));
+      setText('');
     }
+  };
 
-    e.preventDefault();
-  }
-
-  deleteItem(key) {
-    const { items } = this.state;
+  const deleteItem = (key) => {
     const filteredItems = items.filter((item) => item.key !== key);
+    setItems(filteredItems);
+  };
 
-    this.setState({
-      items: filteredItems,
-    });
-  }
-
-  render() {
-    const { items, text } = this.state;
-
-    return (
-      <div className={styles.listElements}>
-        <form onSubmit={this.addItem} className={styles.newItem}>
-          <input value={text} onChange={(e) => this.setState({ text: e.target.value })} placeholder="Item Name" />
-          <button type="submit" aria-label="Add item"><FontAwesomeIcon icon={faPlusCircle} /></button>
-        </form>
-        <ListItems entries={items} deleteItem={this.deleteItem} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.listElements}>
+      <form onSubmit={addItem} className={styles.newItem}>
+        <input value={text} onChange={(e) => setText(e.target.value)} placeholder="Item Name" />
+        <button type="submit" aria-label="Add item"><FontAwesomeIcon icon={faPlusCircle} /></button>
+      </form>
+      <ListItems entries={items} deleteItem={deleteItem} />
+    </div>
+  );
+};
 
 export default ShoppingList;
