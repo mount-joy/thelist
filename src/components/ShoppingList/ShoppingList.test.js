c4cdc7e15aea00d2922920d0fee1a1e5c5b66aa8
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-10-27 08:46:19 +0000

    add tests for ShoppingList (#17)

---
+++ src/components/ShoppingList/ShoppingList.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ShoppingList from './ShoppingList';
+
+describe('ShoppingList', () => {
+  it('renders the add button', () => {
+    const { getByLabelText } = render(<ShoppingList />);
+    const addButton = getByLabelText(/Add/);
+
+    expect(addButton).toBeInTheDocument();
+    expect(addButton.nodeName).toEqual('BUTTON');
+    expect(addButton.type).toEqual('submit');
+  });
+
+  describe('when an item is added to the list', () => {
+    let instance;
+    let inputBox;
+
+    beforeEach(() => {
+      instance = render(<ShoppingList />);
+      const { getByPlaceholderText, container } = instance;
+
+      inputBox = getByPlaceholderText('Item Name');
+      const form = container.querySelector('form');
+
+      fireEvent.change(inputBox, { target: { value: 'Oranges' } });
+      expect(inputBox.value).toBe('Oranges');
+      fireEvent.submit(form);
+      fireEvent.change(inputBox, { target: { value: 'Apples' } });
+      fireEvent.submit(form);
+    });
+
+    it('displays the new items', () => {
+      expect(instance.getByText(/Oranges/)).toBeInTheDocument();
+      expect(instance.getByText(/Apples/)).toBeInTheDocument();
+      expect(inputBox.value).toBe('');
+    });
+
+    it('removes the item when delete is pressed', () => {
+      const deleteButton = instance.getByTestId('delete-item-Oranges');
+      fireEvent.click(deleteButton);
+
+      expect(instance.queryByText(/Oranges/)).toBeNull();
+      expect(instance.queryByText(/Apples/)).not.toBeNull();
+    });
+  });
+});
