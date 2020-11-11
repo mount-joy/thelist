a6d847cebaae37741376407b3f6f219eaf65e487
LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>
2020-11-11 21:57:28 +0000

    lint fixes etc
    empty test file removed

--- src/components/ListHeader/ListHeader.js
+++ src/components/ListHeader/ListHeader.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { ReactComponent as Logo } from '../../static/logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
+import { ReactComponent as Logo } from '../../static/logo.svg';
 
 import styles from './styles.module.css';
 
 const ListHeader = () => (
-    <div className={styles.listHeader}>
-        <div className={styles.headerRow}>
-            <Logo className={styles.logo}
-            />
-            <FontAwesomeIcon
-                icon={faShareAlt}
-                className={styles.shareIcon}
-            />
-        </div>
-        <div className={styles.headerRow}>
-            <h3>Shopping List</h3>
-            <h4>15 items</h4>
-        </div>
+  <div className={styles.listHeader}>
+    <div className={styles.headerRow}>
+      <Logo className={styles.logo} />
+      <FontAwesomeIcon
+        icon={faShareAlt}
+        className={styles.shareIcon}
+      />
     </div>
+    <div className={styles.headerRow}>
+      <h3>Shopping List</h3>
+      <h4>15 items</h4>
+    </div>
+  </div>
 );
 
 export default ListHeader;
