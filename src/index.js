23119952a1c1f74422ff77f15d7210850fa92c8e
LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>
2020-11-12 18:07:41 +0000

    placed components inside single app component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ListHeader from './components/ListHeader';
-import ShoppingList from './components/ShoppingList';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import './style/index.css';
 
-const header = document.querySelector('#header');
-ReactDOM.render(<ListHeader />, header);
-
-const destination = document.querySelector('#wrapper');
-ReactDOM.render(<ShoppingList />, destination);
+const wrapper = document.querySelector('#wrapper');
+ReactDOM.render(<App />, wrapper);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
