5663cfcb515d84eb1618d5622b9f85beb660f895
LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>
2020-11-17 15:19:09 +0000

    lint fix

--- src/components/ShoppingList/ShoppingList.test.js
+++ src/components/ShoppingList/ShoppingList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, getNodeText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import ShoppingList from './ShoppingList';
 
 describe('ShoppingList', () => {
