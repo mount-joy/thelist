0f11e52d3979288d94771ecbeea1ff8095b69a05
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-11-17 22:29:43 +0000

    uncomment tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { getNodeText, render } from '@testing-library/react';
 
 import App from './App';
 
@@ -10,20 +10,23 @@ describe('App', () => {
   });
 
   it('renders the title', () => {
-    expect(instance.getByText('Shopping List')).toBeInTheDocument();
+    const title = instance.getAllByRole('heading')
+      .map((heading) => getNodeText(heading))
+      .find((text) => text === 'Shopping List');
+    expect(title).toBeDefined();
   });
 
   it('renders the number of items on the list', () => {
     expect(instance.getByText(/[0-9]+ items/)).toBeInTheDocument();
   });
 
-  // it('renders the logo', () => {
-  //   expect(instance.getByTitle('Logo')).toBeInTheDocument();
-  // });
+  it('renders the logo', () => {
+    expect(instance.getByTitle('Logo')).toBeInTheDocument();
+  });
 
-  // it('renders the share icon', () => {
-  //   expect(instance.getByLabelText('Share this list')).toBeInTheDocument();
-  // });
+  it('renders the share icon', () => {
+    expect(instance.getByLabelText('Share this list')).toBeInTheDocument();
+  });
 
   it('renders the box to enter new items', () => {
     expect(instance.getByPlaceholderText('Item Name')).toBeInTheDocument();
