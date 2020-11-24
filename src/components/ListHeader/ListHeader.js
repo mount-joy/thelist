3e2d3b9b1ef88ad2b650d06184cf8132ed182604
LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>
2020-11-24 15:04:27 +0000

    share icon toggles visibility of list details

--- src/components/ListHeader/ListHeader.js
+++ src/components/ListHeader/ListHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareAlt, faCopy, faPencilAlt, faClock, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ReactComponent as Logo } from '../../static/logo.svg';
@@ -6,6 +6,8 @@ import { ReactComponent as Logo } from '../../static/logo.svg';
 import styles from './styles.module.css';
 
 const ListHeader = () => {
+  const [listDetailsShown, showListDetails] = useState();
+
   const shareList = () => {
     if (navigator.share) {
       navigator.share({
@@ -28,7 +30,7 @@ const ListHeader = () => {
           className={styles.shareIcon}
           title="Share this list"
           tabIndex={0}
-          onClick={shareList}
+          onClick={() => showListDetails(!listDetailsShown)}
         />
       </div>
       <div className={styles.headerRow}>
@@ -43,42 +45,48 @@ const ListHeader = () => {
         </h3>
         <h4>15 items</h4>
       </div>
-      <div className={styles.subheadingRow}>
-        <h4>
-          <FontAwesomeIcon
-            icon={faClock}
-            className={styles.clockIcon}
-          />
-          Created 1st Jan 2020
-        </h4>
-      </div>
-      <div className={styles.copyList}>
-        <h5>Copy -</h5>
-        <input
-          type="text"
-          spellCheck="false"
-          value="https:thelist.app"
-          id="listText"
-          onClick={shareList}
-        />
-        <FontAwesomeIcon
-          icon={faCopy}
-          className={styles.copyIcon}
-          title="Copy list"
-          tabIndex={0}
-          onClick={shareList}
-        />
-      </div>
-      <div
-        className={styles.deleteList}
-        title="Delete list"
-      >
-        <FontAwesomeIcon
-          icon={faTrash}
-          className={styles.deleteListIcon}
-        />
-        Delete List
-      </div>
+      {listDetailsShown ? (
+        <>
+          <div className={styles.subheadingRow}>
+            <h4>
+              <FontAwesomeIcon
+                icon={faClock}
+                className={styles.clockIcon}
+              />
+              Created 1st Jan 2020
+            </h4>
+          </div>
+          <div className={styles.copyList}>
+            <h5>Copy -</h5>
+            <input
+              type="text"
+              spellCheck="false"
+              value="https:thelist.app"
+              id="listText"
+              onClick={shareList}
+            />
+            <FontAwesomeIcon
+              icon={faCopy}
+              className={styles.copyIcon}
+              title="Copy list"
+              tabIndex={0}
+              onClick={shareList}
+            />
+          </div>
+          <div
+            className={styles.deleteList}
+            title="Delete list"
+          >
+            <FontAwesomeIcon
+              icon={faTrash}
+              className={styles.deleteListIcon}
+            />
+            Delete List
+          </div>
+        </>
+      ) : (
+        null
+      )}
     </div>
   );
 };
