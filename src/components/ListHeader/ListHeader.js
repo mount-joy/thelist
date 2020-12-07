19891877255d47872a78429b37ebb4c695a020e8
Joseph Cashmore <joseph.cashmore87@gmail.com>
2020-12-07 08:47:42 +0000

    Share list initialisation (#66)
    * font size adjustments
    
    * padding adjustments
    
    * updates to "completed" styling
    
    I couldn't find a scenario with the use of a line separator that I liked, the heading is better.
    
    * made new item form text more generic
    
    * test fixes
    
    * attempting an "isEditable" state to toggle different views
    
    Getting some weird behaviours when item should lose focus.
    
    * remove unnecessary style code
    
    * test fix
    
    * lint fixes
    
    * new edit mode method (thanks sam!)
    
    I found that the delay needed to be a certain length for this to work. All functions fine but not sure if there are ways of minimising the time
    
    * fixed test
    
    This feels like a bit of a cheat. The test isn't able to find the delete button in the current version, but if the element exists even with the class display none, it can be found
    
    * web share or copy to clipboard if statement
    
    far from being react appropriate, but is a start
    
    * Styling the list details/share/delete header
    
    I got carried away...
    
    * share icon cursor pointer
    
    * share icon toggles visibility of list details
    
    * removed fixed height, changed copy to share?
    
    * pencil icon now reveals list details
    
    Share icon is now for sharing only, but if web share api isn't available the copy doesn't currently work - nothing to copy!
    
    * make share function more "react" like
    
    * test fixing
    
    This test seemed to fail randomly...
    
    * modernising js copy to clipboard
    
    * Revert to main version
    
    Test failed for some reason
    
    Co-authored-by: LAPTOP-R2LRJ8HV\josep <joseph@dhproductions.co.uk>

--- src/components/ListHeader/ListHeader.js
+++ src/components/ListHeader/ListHeader.js
@@ -1,26 +1,92 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
+import { faShareAlt, faCopy, faPencilAlt, faClock, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ReactComponent as Logo } from '../../static/logo.svg';
 
 import styles from './styles.module.css';
 
-const ListHeader = () => (
-  <div className={styles.listHeader}>
-    <div className={styles.headerRow}>
-      <Logo className={styles.logo} title="Logo" />
-      <FontAwesomeIcon
-        icon={faShareAlt}
-        className={styles.shareIcon}
-        title="Share this list"
-        tabIndex={0}
-      />
+const ListHeader = () => {
+  const [listDetailsShown, showListDetails] = useState();
+  const listText = useRef(null);
+  const url = 'https://thelist.app';
+
+  const shareList = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: 'Share Your List!',
+        url: 'https:thelist.app',
+      });
+    } else if (navigator.clipboard) {
+      navigator.clipboard.writeText(url);
+    }
+  };
+
+  return (
+    <div className={styles.listHeader}>
+      <div className={styles.headerRow}>
+        <Logo className={styles.logo} title="Logo" />
+        <FontAwesomeIcon
+          icon={faShareAlt}
+          className={styles.shareIcon}
+          title="Share this list"
+          tabIndex={0}
+          onClick={shareList}
+        />
+      </div>
+      <div className={styles.headerRow}>
+        <h3>
+          Shopping List
+          <FontAwesomeIcon
+            icon={faPencilAlt}
+            className={styles.pencilIcon}
+            title="Edit list name"
+            tabIndex={0}
+            onClick={() => showListDetails(!listDetailsShown)}
+          />
+        </h3>
+        <h4>15 items</h4>
+      </div>
+      <div className={listDetailsShown ? (styles.displayListDetails) : (styles.hideListDetails)}>
+        <div className={styles.subheadingRow}>
+          <h4>
+            <FontAwesomeIcon
+              icon={faClock}
+              className={styles.clockIcon}
+            />
+            Created 1st Jan 2020
+          </h4>
+        </div>
+        <div className={styles.copyList}>
+          <h5>Share -</h5>
+          <input
+            type="text"
+            spellCheck="false"
+            value={url}
+            ref={listText}
+            onClick={shareList}
+            readOnly
+          />
+          <FontAwesomeIcon
+            icon={faCopy}
+            className={styles.copyIcon}
+            title="Copy list"
+            tabIndex={0}
+            onClick={shareList}
+          />
+        </div>
+        <div
+          className={styles.deleteList}
+          title="Delete list"
+        >
+          <FontAwesomeIcon
+            icon={faTrash}
+            className={styles.deleteListIcon}
+          />
+          Delete List
+        </div>
+      </div>
     </div>
-    <div className={styles.headerRow}>
-      <h3>Shopping List</h3>
-      <h4>15 items</h4>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ListHeader;
