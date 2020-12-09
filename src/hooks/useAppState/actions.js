1b66f0a9c108279b3ddd5899fa66d0a0728585f7
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-12-09 18:58:46 +0000

    list switching (#74)

--- src/hooks/useAppState/actions.js
+++ src/hooks/useAppState/actions.js
@@ -2,6 +2,7 @@ import { types } from './reducer';
 
 const getActions = (dispatch) => ({
   setState: (state) => dispatch({ type: types.SET_STATE, data: { state } }),
+  switchList: (newIndex) => dispatch({ type: types.SWITCH_LIST, data: { newIndex } }),
   newItem: (text, id) => dispatch({ type: types.NEW_ITEM, data: { text, id, key: `${Date.now()}` } }),
   newList: (name, id) => dispatch({ type: types.NEW_LIST, data: { name, id, key: `${Date.now()}` } }),
   deleteItemByKey: (key) => dispatch({ type: types.DELETE_ITEM_BY_KEY, data: { key } }),
