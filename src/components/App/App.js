b29709b5756fb159c283a210b67a1476bd054cc6
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-12-10 23:27:17 +0000

    Set page title based on list name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ListHeader from '../ListHeader';
 import ShoppingList from '../ShoppingList';
 import ListSelector from '../ListSelector';
 import useAppState from '../../hooks/useAppState';
-import { selectItems, selectLists } from '../../hooks/useAppState/selectors';
+import { selectItems, selectListName, selectLists } from '../../hooks/useAppState/selectors';
+import getPageTitle from '../../utils/getPageTitle';
 
 const App = () => {
   const [state, actions] = useAppState();
+
+  useEffect(() => {
+    const name = selectListName(state);
+    document.title = getPageTitle(name);
+  }, [state]);
 
   return (
     <>
