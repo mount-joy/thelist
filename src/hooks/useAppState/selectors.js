282bbeea4f4fdfb2b46be74204352e9771256634
Sam Stradling <9036408+samstradling@users.noreply.github.com>
2020-12-14 19:17:53 +0000

    Support sharing lists (#78)

--- src/hooks/useAppState/selectors.js
+++ src/hooks/useAppState/selectors.js
@@ -1,3 +1,4 @@
 export const selectItems = (state) => state.lists[state.index]?.items ?? [];
 export const selectLists = (state) => state.lists ?? [];
-export const selectListId = (state) => state.lists[state.index]?.id ?? null;
+export const selectList = (state) => state.lists[state.index] ?? null;
+export const selectListId = (state) => selectList(state)?.id ?? null;
